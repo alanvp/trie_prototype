fc57388d0239847db0eb93bbf9480986de8fda21
alanvp <apvanpelt@yahoo.com>
2014-03-09 14:08:10 -0700

    got results to display. no handlebars

--- public/app.js
+++ public/app.js
@@ -4,18 +4,69 @@ window.App = {
   Views: {},
   Routers: {},  
   initialize: function() {
-    this.router = new this.Routers.Main();
-    Backbone.history.start({pushState: true});
-
     App.autocompleter = new Autocompleter();
     var ws = new WebSocket('ws://' + window.location.host + window.location.pathname);
     ws.onmessage = function(m) { 
-      autocompleter.add(m.data); 
+      App.autocompleter.add(m.data); 
     };
-
+    this.router = new this.Routers.Main();
+    Backbone.history.start({pushState: true});
   }
 };
 $(document).ready(function(){
   App.initialize();
+});
+
+
+App.Routers.Main = Backbone.Router.extend({
+  routes: {
+    "": "index",
+//    ":query": "wordSearch"
+  },
+  index: function() {
+    var view = new App.Views.Index();
+    $('#container').html(view.render().el);
+  },
+
+  // wordSearch: function(query) {
+  //   console.log("Yoo!"+query);
+  //   var view = new App.Views.Index();
+  //   $('#container').html(view.render().el);
+  //   view.autoSearch(query);
+  // }
+
+});
+
+
+App.Views.Index = Backbone.View.extend({
+  tagName: "span",
+  template: function(){ return "<form id=\"searchForm\"> <input id=\"searchBox\" type=\"text\"> Search field</input></form>";},
+//  template: HandlebarsTemplates['/index'],
+  events: {
+    'keyup #searchBox': 'renderResults',
+    'submit #searchForm': 'renderResults' 
+  },
+  render: function() {
+    $(this.el).html(this.template());
+//    this.render_results();
+    return this;
+  },
+  renderResults: function(event) {
+    event.preventDefault();
+    $('#results').empty();
+    var results = App.autocompleter.complete($('#searchBox').val());
+    results.forEach(function(result) {
+      $('#results').append("<li><a href=\"https://en.wikipedia.org/wiki/"+ result + "\">"+ result + "</li>");
+    });
+  },
+    
+  // autoSearch: function(query){
+  //   $('#results').empty();
+  //   var results = App.autocompleter.complete(query);
+  //   $.each(results, function(index, result){
+  //       $("#results").append("<li><a href=\"https://en.wikipedia.org/wiki/"+ result + "\">"+ result + "</li>");
+     
+  //   });
+  // }
 });
 
--- public/trie.js
+++ public/trie.js
@@ -1,21 +1,18 @@
 Trie = function(){
   this.characters = {};
+  this.isWord = false;
 };
 
-Trie.prototype.learn = function(word, index){
-  // This function should add the given word,
-  // starting from the given index,
-  // to this Trie.
-
-  // It will be recursive.  It will tell
-  // the correct child of this Trie to learn the word
-  // starting from a later index.
-
-  // Consider what the learn function should do
-  // when it reaches the end of the word?
-  // A word does not necessarily end at a leaf.
-  // You must mark nodes which are the ends of words,
-  // so that the words can be reconstructed later.
+Trie.prototype.learn = function(word){
+  if (word.length === 0) {
+    this.isWord = true;
+    return;
+  }
+  char = word.slice(0,1);
+  if (this.characters[char] === undefined) {
+    this.characters[char] = new Trie();
+  } 
+  this.characters[char].learn(word.slice(1, word.length));
 };
 
 Trie.prototype.getWords = function(words, currentWord){
@@ -23,17 +20,48 @@ Trie.prototype.getWords = function(words, currentWord){
   // contained in this Trie.
   // it will use currentWord as a prefix,
   // since a Trie doesn't know about its parents.
+  words = words || [];
+  currentWord = currentWord || "";
+
+  if (this.isWord === true) {
+    words.push(currentWord);
+  } 
+  for (var key in this.characters) {
+   words = words.concat(this.characters[key].getWords([],currentWord+key));
+  }  
+  return words;
 };
 
-Trie.prototype.find = function(word, index){
+Trie.prototype.find = function(word){
   // This function will return the node in the trie
   // which corresponds to the end of the passed in word.
-
   // Be sure to consider what happens if the word is not in this Trie.
+  if (word.length === 0) {
+    return this;
+  }
+  var char = word.slice(0,1);
+  if (this.characters[char] === undefined) {
+    return;
+  } 
+  return this.characters[char].find(word.slice(1, word.length));
+
 };
 
 Trie.prototype.autoComplete = function(prefix){
   // This function will return all completions 
   // for a given prefix.
   // It should use find and getWords.
+  var prefixNode = this.find(prefix);
+  if (prefixNode === undefined) {return [];}
+  var suffixArr = prefixNode.getWords();
+  var resultsArr = [];
+  suffixArr.forEach(function(suffix) {
+    resultsArr.push(prefix+suffix);
+  });
+  return resultsArr;
 };
+
+// var a = ["a", "b", "c"];
+// a.forEach(function(entry) {
+//     console.log(entry);
+// });
